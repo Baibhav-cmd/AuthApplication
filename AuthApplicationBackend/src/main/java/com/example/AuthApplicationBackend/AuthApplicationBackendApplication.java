87babfbf8b84fbd789594e587cf694ff451072ca
Baibhav <baibhavthapa12@gmail.com>
2026-05-16 21:55:50 +0545

    Initial commit

---
+++ AuthApplicationBackend/src/main/java/com/example/AuthApplicationBackend/AuthApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthApplicationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ AuthApplicationBackend/src/test/java/com/example/AuthApplicationBackend/AuthApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthApplicationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
